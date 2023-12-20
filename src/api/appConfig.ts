2de02cf8182fd28bfee0a7b8776a1a4e0f6de114
Ronen Mars <ronen@autokitteh.com>
2023-12-20 13:33:15 +0200

    refactor: baseUrl string fix

--- src/api/appConfig.ts
+++ src/api/appConfig.ts
@@ -1,15 +1,7 @@
 import { BASE_URL } from "@constants";
 
 export const baseApi = BASE_URL;
-const urlPrefix = "autokitteh.";
 export const appConfig = {
 	accessToken: "<KEY>",
-	projectApiBase: `${baseApi}/${urlPrefix}projects.v1.ProjectsService`,
-	connectionApiBase: `${baseApi}/${urlPrefix}connections.v1.ConnectionsService`,
-	environmentApiBase: `${baseApi}/${urlPrefix}envs.v1.EnvsService`,
-	organizationApiBase: `${baseApi}/${urlPrefix}orgs.v1.OrgsService`,
-	integrationApiBase: `${baseApi}/${urlPrefix}integrations.v1.IntegrationsService`,
-	userApiBase: `${baseApi}/${urlPrefix}users.v1.UsersService`,
 	manifestApiBase: baseApi,
-	deploymentApiBase: `${baseApi}/${urlPrefix}deployments.v1.DeploymentsService`,
 };
