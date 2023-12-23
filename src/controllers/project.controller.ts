97b00864f8e3c780bcf3eefc4b29f609ee3a76e9
Ronen Mars <ronen@autokitteh.com>
2023-12-24 01:29:07 +0200

    refactor: remove unused import

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,4 +1,3 @@
-import { log } from "console";
 import { Deployment } from "@ak-proto-ts/deployments/v1/deployment_pb";
 import { Project } from "@ak-proto-ts/projects/v1/project_pb";
 import { Session } from "@ak-proto-ts/sessions/v1/session_pb";
