51dcaced8ebc2d2beed1cb950c1e1aeea7f58697
Ronen Mars <ronen@autokitteh.com>
2023-12-23 17:58:21 +0200

    fix: rename file

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -2,4 +2,4 @@ export { getNonce } from "@utilities/getNonce.utils";
 export { getTheme } from "@utilities/getTheme.utils";
 export { getUri } from "@utilities/getUri.utils";
 export { flattenArray } from "@utilities/flattenArray.utils";
-export { ValidateURL } from "@utilities/validateUrl.utils1";
+export { ValidateURL } from "@utilities/validateUrl.utils";
