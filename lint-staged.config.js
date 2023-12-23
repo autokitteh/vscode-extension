ccbe33a810fbacc360c73c133d6890fedfabfaca
Ronen Mars <ronen@autokitteh.com>
2023-12-23 18:39:12 +0200

    feat: linting check and validation on pre-commit hook

---
+++ lint-staged.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable unicorn/filename-case */
+module.exports = {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    '*.{ts,tsx}': (filenames) => [
+      'npm run format:fix',
+      'npm run validate',
+    ],
+  };
