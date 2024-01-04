fea9426b69f8e159478f87489018c2920988c00d
Ronen Mars <ronen@autokitteh.com>
2024-01-04 12:22:10 +0200

    process session history

--- src/services/sessions.service.ts
+++ src/services/sessions.service.ts
@@ -7,6 +7,7 @@ import { Session, SessionHistory } from "@type/models";
 import { ServiceResponse } from "@type/services.types";
 import { flattenArray } from "@utilities";
 import { get } from "lodash";
+import { window } from "vscode";
 
 export class SessionsService {
 	static async listByEnvironmentId(environmentId: string): Promise<ServiceResponse<Session[]>> {
@@ -21,8 +22,78 @@ export class SessionsService {
 
 	static async getSessionHistory(sessionId: string): Promise<ServiceResponse<SessionHistory>> {
 		try {
+			let output = window.createOutputChannel("autokitteh");
+			output.clear();
+
 			const response = await sessionsClient.getHistory({ sessionId });
 			const sessionHistory = convertSessionHistoryProtoToModel(response.history?.states);
+			type StateValue = {
+				runId?: string;
+				prints?: string[];
+				call?: {
+					type: {
+						value: {
+							signature: string;
+							argNames: string[];
+							name: string;
+						};
+					};
+				};
+				exports?: any;
+			};
+
+			type State = {
+				case: string;
+				value: StateValue;
+			};
+
+			type Timestamp = {
+				seconds: number | bigint;
+				nanos: number;
+			};
+
+			type Entry = {
+				states: State;
+				t: Timestamp;
+			};
+
+			type Data = {
+				states: Entry[];
+				calls: any[];
+			};
+
+			sessionHistory.forEach((entry) => {
+				const stateCase = entry.states.case;
+				const value = entry.states.value as any;
+
+				output.appendLine(`Status: ${stateCase}\r\n`);
+
+				if (stateCase === "running" || stateCase === "completed") {
+					// Print function call if available
+					if (value.call) {
+						const callInfo = value.call.type.value;
+						const functionCall = `${callInfo.name}(${callInfo.argNames.join(", ")})`;
+						output.appendLine(`Function Call: ${functionCall}\r\n`);
+					}
+
+					// Print 'prints' array contents
+					if (value.prints && value.prints.length > 0) {
+						output.appendLine("Prints:\r\n");
+						value.prints.forEach((print: string) => {
+							output.appendLine(`  ${print}\r\n`);
+						});
+					}
+				}
+
+				if (stateCase === "error") {
+					const errorMessage = value?.error.message;
+					output.appendLine(`Errors: ${errorMessage}\r\n`);
+				}
+
+				output.appendLine("~~~~~~~~~~~~~~~~~~"); // Blank line for separation
+			});
+			output.show();
+
 			return { data: undefined, error: undefined };
 		} catch (error) {
 			return { data: undefined, error };
