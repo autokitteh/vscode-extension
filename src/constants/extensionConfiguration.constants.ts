aa50a644c9c0902b67e930feccfef7d01315205e
Ronen Mars <ronen@autokitteh.com>
2024-01-08 09:30:19 +0200

    revert: change the code back to the main values

--- src/constants/extensionConfiguration.constants.ts
+++ src/constants/extensionConfiguration.constants.ts
@@ -1,8 +1,8 @@
 export const DEFAULT_USERNAME = "anonymous";
 export const DEFAULT_SERVER_URL = "http://localhost:9980";
 export const EXT_PUBLISHER = "undefined_publisher";
-export const DEFAULT_SIDEBAR_VIEW_REFRESH_INTERVAL = 1500;
-export const DEFAULT_PROJECT_VIEW_REFRESH_INTERVAL = 500;
+export const DEFAULT_SIDEBAR_VIEW_REFRESH_INTERVAL = 1000;
+export const DEFAULT_PROJECT_VIEW_REFRESH_INTERVAL = 1000;
 export const DEFAULT_ENVIRONMENT = "default";
 export const DEFAULT_CONNECTION_TEST_INTERVAL = 5000;
 export const DEFAULT_CONNECTION_TEST_SLOW_INTERVAL = 300000;
