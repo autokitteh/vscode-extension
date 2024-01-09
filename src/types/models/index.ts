f40b7e2cdf33d1d3a498eb449a2da1c3353e1335
Ronen Mars <ronen@autokitteh.com>
2024-01-09 14:51:34 +0200

    refactor(UI-29): remove unused file

--- src/types/models/index.ts
+++ src/types/models/index.ts
@@ -1,5 +1,4 @@
 export type { Deployment } from "@type/models/deployment.type";
 export type { Project } from "@type/models/project.type";
 export type { Session } from "@type/models/session.type";
-export type { SessionHistory } from "@type/models/sessionHistory.type";
 export type { ProtoSessionHistoryState } from "@type/models/protoSessionHistoryState.type";
--- src/types/models/sessionHistory.type.ts
+++
@@ -1,3 +0,0 @@
-export type SessionHistory = {
-	states?: Array<object>;
-};
