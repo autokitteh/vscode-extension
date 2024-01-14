030c4449624efba4d494b2c91330bea32a0af220
RonenMars <ronen@autokitteh.com>
2024-01-14 13:48:52 +0200

    feat(UI-48): remove username and authentication from the system (#49)

--- src/constants/extensionConfiguration.constants.ts
+++ src/constants/extensionConfiguration.constants.ts
@@ -1,4 +1,3 @@
-export const DEFAULT_USERNAME = "anonymous";
 export const DEFAULT_SERVER_URL = "http://localhost:9980";
 export const EXT_PUBLISHER = "undefined_publisher";
 export const DEFAULT_SIDEBAR_VIEW_REFRESH_INTERVAL = 1000;
--- src/vscommands/walkthrough/index.ts
+++ src/vscommands/walkthrough/index.ts
@@ -1,3 +1,2 @@
-export { setUsername, openUsernameInputDialog } from "@vscommands/walkthrough/username.vscommands";
 export { setBaseURL, openBaseURLInputDialog } from "@vscommands/walkthrough/server.vscommands";
 export { openWalkthrough } from "@vscommands/walkthrough/openWalkthrough.vscommands";
