105527fa7a2e723110b1459d297c73532a071590
RonenMars <ronen@autokitteh.com>
2024-01-15 10:44:39 +0200

    refactor: clear redundant code (#59)

--- src/types/interfaces/callback.interface.type.ts
+++ src/types/interfaces/callback.interface.type.ts
@@ -1,7 +1,4 @@
-import { EntitySectionRowsRange } from "@type/views/webview";
-
 export type Callback = () => void;
 export type ProjectCB = (projectId: string) => void;
 export type CallbackWStringIdParam = (id: string) => void;
-export type SectionRowsRangeCB = (value: EntitySectionRowsRange) => void;
 export type SectionRowsRange = { startIndex: number; endIndex: number };
