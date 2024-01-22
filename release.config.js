5a58a8ed03abd5e16e5367c38111af9e3061429b
RonenMars <ronen@autokitteh.com>
2024-01-22 14:12:40 +0200

    chore: add funny messages on release success/fail (#85)
    * ci: add funny messages on release success/fail
    
    * ci: update release messages for the slack bot

--- release.config.js
+++ release.config.js
@@ -46,6 +46,35 @@ module.exports = {
                 "notifyOnSuccess": false,
                 "notifyOnFail": false,
                 "slackWebhook": process.env.SLACK_WEBHOOK_URL,
+                "onSuccessTemplate": {
+                    "text": 
+                    ":partying_face: *Meow-velous News, Team!* :paw_prints: \n"+
+                    "Version *$npm_package_version* has just landed on our digital doorstep, "+
+                    "and it's purr-fectly packed with features!\n\n \n\n"+
+                    ":yasss_cat: What's New:\n"+
+                    "$release_notes	\n\n"+
+                    ":announcement: *A Paws-itive Reminder:*\n"+
+                    "> Don't forget to update your systems! It's easier than herding cats,"+
+                    "we promise. Just click here: $repo_url, download and install the latest release and enjoy the "+
+                    "latest features.\n\n*Happy Coding, Furr-iends!* "+
+                    "Let's make some paw-some progress!\n\n"+
+                    ":cat-roomba-exceptionally-fast: "+
+                    "*Stay Pawsome!*\n :catjam: _Your Dev Team_ :catjam:"
+                },
+                "onFailTemplate": {
+                    "text":
+                    ":red_circle: *Attention Team: Paws for Thought!* :paw_prints:\n"+
+                    "Our latest version *$npm_package_version* has encountered a hiccup,"+
+                    " and it's not as purr-fect as we hoped.\n\n"+
+                    ":toolbox: *What We're Doing:*\n- Assembling our team of "+
+                    "Cat Coders to hunt down those pesky bugs. Link to our rrrrrrepo: $repo_url \n"+
+                    "- Refining the 'cat-nip' module to ensure it's not too overwhelming.\n\n"+
+                    " :announcement: *What You Can Do:*\n> Please hold off on updating your systems. "+
+                    "We're herding these cats as fast as we can and will let you know the moment "+
+                    "it's safe to pounce on the update button.\n\n*Thank You for Your Patience!* "+
+                    "We're working hard to make things right and appreciate your support.\n\n"+
+                    " :sob_cat: *Stay Tuned for Updates!*\n :catjam: _Your Dev Team_ :catjam:"
+                },
                 "branchesConfig": [
                     {
                         "pattern": "main",
