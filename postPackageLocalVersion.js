f71c97c80fe5098e2152692cf37ae4774399cee8
Ronen Mars <ronen@autokitteh.com>
2024-01-23 10:50:04 +0200

    style: apply prettier on the missing files

--- postPackageLocalVersion.js
+++ postPackageLocalVersion.js
@@ -1,15 +1,17 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const packageJsonPath = './package.json';
+const packageJsonPath = "./package.json";
 const packageJson = require(packageJsonPath);
 
-let versionComponents = packageJson.version.split('-');
+let versionComponents = packageJson.version.split("-");
 if (versionComponents.length > 1) {
-    packageJson.version = versionComponents[0];
-    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-    console.log(`Updated base version in package.json back to:${packageJson.version}`);
-    return;
-} 
+	packageJson.version = versionComponents[0];
+	fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+	console.log(`Updated base version in package.json back to:${packageJson.version}`);
+	return;
+}
 
-throw new Error("Invalid version format in package.json. "+
-    "The version field is either empty or not properly formatted. Expected format: 'x.y.z-hash'.");
+throw new Error(
+	"Invalid version format in package.json. " +
+		"The version field is either empty or not properly formatted. Expected format: 'x.y.z-hash'."
+);
