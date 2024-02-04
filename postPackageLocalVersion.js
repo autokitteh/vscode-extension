67a50ee232c070648837aa18db64639babec11ef
Ronen Mars <ronen@autokitteh.com>
2024-02-04 15:29:36 +0200

    fix: local package npm script

--- postPackageLocalVersion.js
+++ postPackageLocalVersion.js
@@ -3,9 +3,19 @@ const fs = require("fs");
 const packageJsonPath = "./package.json";
 const packageJson = require(packageJsonPath);
 
-let versionComponents = packageJson.version.split("-");
-if (versionComponents.length > 1) {
-	packageJson.version = versionComponents[0];
+function getStringUntilLastHyphen(str) {
+	const lastHyphenIndex = str.lastIndexOf("-");
+	// If there's no hyphen, return the original string
+	if (lastHyphenIndex === -1) {
+		return str;
+	}
+	return str.substring(0, lastHyphenIndex);
+}
+
+let versionComponents = getStringUntilLastHyphen(packageJson.version);
+
+if (versionComponents.length) {
+	packageJson.version = versionComponents;
 	fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 	console.log(`Updated base version in package.json back to:${packageJson.version}`);
 	return;
