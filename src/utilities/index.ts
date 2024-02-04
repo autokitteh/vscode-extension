0ade36c0b6d57560ce1a4863583fe203980327bd
Ronen Mars <ronen@autokitteh.com>
2024-02-04 15:12:51 +0200

    feat: starlark file download and extract code optimization

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -6,3 +6,4 @@ export { flattenArray } from "@utilities/flattenArray.utils";
 export { ValidateURL } from "@utilities/validateUrl.utils";
 export { convertTimestampToDate } from "@utilities/convertTimestampToDate.utils";
 export { sortArray } from "@utilities/sortArray.utils";
+export { extractArchive } from "@utilities/archive.utils";
--- src/utilities/starlark/starlark.utils.ts
+++ src/utilities/starlark/starlark.utils.ts
@@ -1,111 +1,13 @@
-import { exec } from "child_process";
 import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
-import * as util from "util";
-import * as zlib from "zlib";
 import { namespaces, starlarkExecutableGithubRepository, vsCommands } from "@constants";
 import { translate } from "@i18n";
 import { Asset, AssetInfo, GitHubRelease } from "@interfaces";
 import { LoggerService } from "@services";
-import AdmZip from "adm-zip";
+import { extractArchive } from "@utilities";
 import axios from "axios";
-import * as tarFs from "tar-fs";
 import { commands, workspace, window, ExtensionContext } from "vscode";
-
-const execPromise = util.promisify(exec);
-
-function getArchiveType(filename: string): string {
-	const ext = path.extname(filename).toLowerCase();
-	if (ext === ".xz") {
-		return "tar.xz";
-	}
-	if (ext === ".gz") {
-		return "tar.gz";
-	}
-	if (ext === ".tar") {
-		return "tar";
-	}
-	if (ext === ".zip") {
-		return "zip";
-	}
-	return "unknown";
-}
-
-async function extractXzFile(inputPath: string, outputPath: string): Promise<string | undefined> {
-	if (!fs.existsSync(outputPath)) {
-		fs.mkdirSync(outputPath, { recursive: true });
-	}
-
-	const outputFilePath = path.join(outputPath, path.basename(inputPath, ".xz"));
-	const command = `xz -d -k -f -c "${inputPath}" > "${outputFilePath}"`;
-
-	try {
-		await execPromise(command);
-		console.log(`Successfully extracted ${inputPath} to ${outputFilePath}`);
-		return outputFilePath;
-	} catch (error) {
-		console.error("Extraction failed:", error);
-		return undefined;
-	}
-}
-
-function extractTarGz(inputPath: string, outputPath: string): Promise<void> {
-	return new Promise((resolve, reject) => {
-		const decompressor = zlib.createGunzip();
-		const input = fs.createReadStream(inputPath);
-		const output = tarFs.extract(outputPath);
-
-		input.pipe(decompressor).pipe(output).on("finish", resolve).on("error", reject);
-	});
-}
-
-// Extract tar
-function extractTar(inputPath: string, outputPath: string): Promise<void> {
-	return new Promise((resolve, reject) => {
-		const input = fs.createReadStream(inputPath);
-		const output = tarFs.extract(outputPath);
-
-		input.pipe(output).on("finish", resolve).on("error", reject);
-	});
-}
-
-// Extract zip
-function extractZip(inputPath: string, outputPath: string): Promise<void> {
-	return new Promise((resolve, reject) => {
-		try {
-			const zip = new AdmZip(inputPath);
-			zip.extractAllTo(outputPath, true);
-			resolve();
-		} catch (error) {
-			reject(error);
-		}
-	});
-}
-
-async function extractArchive(inputPath: string, outputPath: string) {
-	const type = getArchiveType(inputPath);
-
-	switch (type) {
-		case "tar.xz":
-			const tarPath = await extractXzFile(inputPath, outputPath);
-			if (tarPath) {
-				return extractTar(tarPath, outputPath);
-			}
-			break;
-		case "tar.gz":
-			return extractTarGz(inputPath, outputPath);
-			break;
-		case "tar":
-			return extractTar(inputPath, outputPath);
-			break;
-		case "zip":
-			return extractZip(inputPath, outputPath);
-			break;
-		default:
-			console.error("Unsupported file type");
-	}
-}
 
 export const getAssetByPlatform = (data: GitHubRelease, platform: string, arch: string): AssetInfo | undefined => {
 	const enrichedPlatform = `autokitteh-starlark-lsp_${platform}_${arch}`;
@@ -171,13 +73,19 @@ export const getNewVersion = async (
 	const fileName = getFileNameFromUrl(release.url);
 	const extensionPath = extensionContext.extensionPath;
 	await downloadAndSaveFile(release.url, `${extensionPath}/${fileName}`);
+	LoggerService.info(namespaces.startlarkLSPServer, translate().t("starlark.executableDownloadedUnpacking"));
+
 	await extractArchive(`${extensionPath}/${fileName}`, extensionPath).catch((error) => {
 		const errorMessage = translate().t("errors.issueExtractLSP", { error: (error as Error).message });
 		LoggerService.error(namespaces.starlarkLSPExecutable, errorMessage);
 		commands.executeCommand(vsCommands.showErrorMessage, namespaces.starlarkLSPExecutable, errorMessage);
 	});
-
 	const lspPath = `${extensionPath}/autokitteh-starlark-lsp`;
+
+	if (!fs.existsSync(lspPath)) {
+		LoggerService.info(namespaces.startlarkLSPServer, translate().t("errors.errorInstallingLSP"));
+		return;
+	}
 
 	extensionContext.workspaceState.update("autokitteh.starlarkLSPPath", lspPath);
 	extensionContext.workspaceState.update("autokitteh.starlarkLSPVersion", release.tag);
@@ -205,6 +113,8 @@ export const downloadExecutable = async (extensionContext: ExtensionContext): Pr
 			"No"
 		);
 		if (userResponse === "Yes") {
+			LoggerService.info(namespaces.startlarkLSPServer, translate().t("starlark.downloadExecutableInProgress"));
+
 			return await getNewVersion(extensionContext, release);
 		}
 	}
