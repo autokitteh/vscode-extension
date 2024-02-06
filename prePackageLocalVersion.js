ecb5a01bd68d130fd4fe469e16fb6899400f3b4f
Ronen Mars <ronen@autokitteh.com>
2024-02-06 08:53:55 +0200

    refactor: remove stash package script

--- prePackageLocalVersion.js
+++
@@ -1,9 +0,0 @@
-const packageJson = require("./package.json");
-const { execSync } = require("child_process");
-const fs = require("fs");
-
-const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
-packageJson.version += `-${commitHash}`;
-
-fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
-console.log(`\n\n\nUpdated hashed version in package.json to: \n${packageJson.version}\n\n\n`);
