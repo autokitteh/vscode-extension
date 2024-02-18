6fe8a806a1e1b78d01a18e225fa852b749a78569
Ronen Mars <ronen@autokitteh.com>
2024-02-18 15:18:25 +0200

    refactor: remove redundat file of enums

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -4,4 +4,3 @@ export { SessionStateType } from "@enums/sessionStates.enum";
 export { LoggerLevel } from "@enums/output.enum";
 export { SortOrder } from "@enums/sortOrder.enum";
 export { ProjectIntervalTypes } from "@enums/projectIntervals.enum";
-export { StarlarkClientModes } from "@enums/starlark.enum";
--- src/enums/starlark.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum StarlarkClientModes {
-	init = 1,
-	running = 2,
-	error = 3,
-}
