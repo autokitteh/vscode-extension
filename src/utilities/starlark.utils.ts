c5eed1356d18eae0eb5ac5a2821284286133bfff
Ronen Mars <ronen@autokitteh.com>
2024-02-21 10:00:26 +0200

    refactor: rewrite lsp service

--- src/utilities/starlark.utils.ts
+++ src/utilities/starlark.utils.ts
@@ -3,9 +3,7 @@ import { ValidateURL } from "@utilities/validateUrl.utils";
 export const isStalarkLSPSocketMode = (path: string): boolean => {
 	if (ValidateURL(path)) {
 		const pathURL = new URL(path);
-		if (pathURL.protocol === "http:" || pathURL.protocol === "https:") {
-			return true;
-		}
+		return pathURL.protocol === "http:" || pathURL.protocol === "https:";
 	}
 	return false;
 };
