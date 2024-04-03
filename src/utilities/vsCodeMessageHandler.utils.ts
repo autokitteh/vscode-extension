e057b29bd82a747a896d15040626e1a271ded978
Ronen Mars <ronen@autokitteh.com>
2024-04-03 14:21:41 +0300

    fix: fix the notification-level getter

--- src/utilities/vsCodeMessageHandler.utils.ts
+++ src/utilities/vsCodeMessageHandler.utils.ts
@@ -3,16 +3,16 @@ import { WorkspaceConfig } from "@utilities/workspaceConfig.util";
 import { window } from "vscode";
 
 export class MessageHandler {
+	private static notificationsLevel = WorkspaceConfig.getFromWorkspace<string>("notificationsLevel", "");
+
 	static infoMessage(messageText: string): void {
-		const notificationsLevel = WorkspaceConfig.getFromWorkspace<string>("notificationsLevel", "");
-		if (notificationsLevel !== "All") {
+		if (MessageHandler.notificationsLevel !== "All") {
 			return;
 		}
 		window.showInformationMessage(messageText);
 	}
 	static errorMessage(messageText: string): void {
-		const notificationsLevel = WorkspaceConfig.getFromWorkspace<string>("notificationsLevel", "");
-		if (notificationsLevel === "None") {
+		if (MessageHandler.notificationsLevel === "None") {
 			return;
 		}
 		window.showErrorMessage(messageText, "View log").then((selection) => {
