cd9f00e8efaf3cc1776de5e6caeb81f126afcac2
Ronen Mars <ronen@autokitteh.com>
2024-04-11 18:47:48 +0300

    feat: delete project

--- vscode-react/src/components/akMonacoEditorModal.component.tsx
+++ vscode-react/src/components/akMonacoEditorModal.component.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { translate } from "@i18n";
 import { Editor } from "@monaco-editor/react";
-import { AKButton, AKModal } from "@react-components";
+import { AKModal } from "@react-components";
 
 export const AKMonacoEditorModal = ({
 	setModal,
@@ -10,7 +9,7 @@ export const AKMonacoEditorModal = ({
 	setModal: (isDisplayed: boolean) => void;
 	content?: string;
 }) => (
-	<AKModal wrapperClasses={["pt-2", "!bg-transparent"]} classes={["bg-[#ffffff85]"]}>
+	<AKModal wrapperClasses={["!bg-transparent"]} classes={["bg-[#ffffff85]", "rounded-none"]}>
 		<div
 			className="flex justify-end cursor-pointer text-black font-extrabold pt-8 text-xl"
 			onClick={() => setModal(false)}
@@ -26,11 +25,6 @@ export const AKMonacoEditorModal = ({
 					theme="vs-dark"
 					options={{ readOnly: true }}
 				/>
-			</div>
-			<div className="flex w-full justify-end mt-2">
-				<AKButton classes="w-full" onClick={() => setModal(false)}>
-					{translate().t("reactApp.deployments.closeModalButton")}
-				</AKButton>
 			</div>
 		</div>
 	</AKModal>
