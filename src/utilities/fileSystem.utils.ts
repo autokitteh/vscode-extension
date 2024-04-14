ae9b3933274f20c02ca12911a4e3b2b076798577
Ronen Mars <ronen@autokitteh.com>
2024-04-15 01:26:08 +0300

    refactor: improve open resources functionality in project settings

--- src/utilities/fileSystem.utils.ts
+++ src/utilities/fileSystem.utils.ts
@@ -55,7 +55,7 @@ export const openFileExplorer = (directoryPath: string): void => {
 	if (!command) {
 		throw new Error(translate().t("errors.notSupportedPlatform"));
 	}
-	const childProcess = spawn(command, [normalizedPath], { stdio: "inherit" });
+	const childProcess = spawn.sync(command, [normalizedPath], { stdio: "inherit" });
 	childProcess.on("error", (error: Error) => {
 		throw new Error((error as Error).message);
 	});
