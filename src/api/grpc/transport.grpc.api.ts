6e49bf8cb9752318ed6190a6c872b55103951c94
Ronen Mars <ronen@autokitteh.com>
2024-05-05 13:59:40 +0300

    feat: authentication token

--- src/api/grpc/transport.grpc.api.ts
+++ src/api/grpc/transport.grpc.api.ts
@@ -1,6 +1,16 @@
-import { jwtInterceptor } from "@api/grpc/jwtInterceptor.grpc.api";
+import { Interceptor } from "@connectrpc/connect";
 import { createConnectTransport } from "@connectrpc/connect-node";
 import { BASE_URL } from "@constants/api.constants";
+import { WorkspaceConfig } from "@utilities";
+
+export const jwtInterceptor: Interceptor = (next) => async (req) => {
+	const authToken = WorkspaceConfig.getFromWorkspace<string>("authToken", "");
+
+	if (authToken) {
+		req.header.set("Authorization", `Bearer ${authToken}`);
+	}
+	return await next(req);
+};
 
 export const grpcTransport = createConnectTransport({
 	baseUrl: BASE_URL,
