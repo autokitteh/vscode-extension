1b841b90cbc5334d6aac557b86a84411b4fe6a31
Ronen Mars <ronen@autokitteh.com>
2024-05-06 14:03:26 +0300

    feat: app loader - context

--- vscode-react/src/components/aKLogo.component.tsx
+++ vscode-react/src/components/aKLogo.component.tsx
@@ -3,6 +3,7 @@ import { Theme } from "@enums";
 import AKLogoBlack from "@react-assets/images/logo/ak-logo-black.svg?react";
 import AKLogoWhite from "@react-assets/images/logo/ak-logo-white.svg?react";
 import { useAppState } from "@react-context/appState.context";
+import { useDelayedLoading } from "@react-hooks";
 import { cn } from "@react-utilities/cnClasses.utils";
 
 type LogoProperties = {
@@ -12,7 +13,8 @@ type LogoProperties = {
 
 export const AKLogo = ({ className, themeVisualType }: LogoProperties): ReactNode => {
 	const [{ loading }] = useAppState();
-	const logoClass = cn(className, { loading });
+	const delayedLoading = useDelayedLoading(loading, 1000);
+	const logoClass = cn(className, { loading: delayedLoading });
 
 	return themeVisualType === 2 || themeVisualType === 3 ? (
 		<AKLogoWhite className={logoClass} fill="white" />
