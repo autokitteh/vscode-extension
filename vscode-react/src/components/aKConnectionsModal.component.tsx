6d01738a6febab37ccf2fa2c06010d462a525f4f
Ronen Mars <ronen@autokitteh.com>
2024-05-08 15:20:43 +0300

    feat: connections management

--- vscode-react/src/components/aKConnectionsModal.component.tsx
+++ vscode-react/src/components/aKConnectionsModal.component.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { translate } from "@i18n";
 import { AKModal } from "@react-components";
-import { AKTable, AKTableCell, AKTableHeader, AKTableHeaderCell, AKTableRow } from "@react-components/AKTable";
+import {
+	AKTable,
+	AKTableCell,
+	AKTableHeader,
+	AKTableHeaderCell,
+	AKTableMessage,
+	AKTableRow,
+} from "@react-components/AKTable";
+import { useAppState } from "@react-context";
 import { Connection } from "@type/models";
 
 export const AKConnectionsModal = ({
@@ -10,6 +19,8 @@ export const AKConnectionsModal = ({
 	onCloseClicked: () => void;
 	connections?: Connection[];
 }) => {
+	const [{ delayedLoading }] = useAppState();
+
 	return (
 		<AKModal wrapperClasses={["!bg-white z-50"]} classes={["bg-black-semi-transparent", "rounded-none"]}>
 			<div
@@ -18,18 +29,23 @@ export const AKConnectionsModal = ({
 			>
 				X
 			</div>
-			<AKTable>
-				<AKTableHeader>
-					<AKTableHeaderCell>Name</AKTableHeaderCell>
-					<AKTableHeaderCell>ID</AKTableHeaderCell>
-				</AKTableHeader>
-				{connections?.map((connection) => (
-					<AKTableRow key={connection.connectionId}>
-						<AKTableCell>{connection.name}</AKTableCell>
-						<AKTableCell>{connection.connectionId}</AKTableCell>
-					</AKTableRow>
-				))}
-			</AKTable>
+			{!delayedLoading && connections?.length ? (
+				<AKTable>
+					<AKTableHeader>
+						<AKTableHeaderCell>Name</AKTableHeaderCell>
+						<AKTableHeaderCell>ID</AKTableHeaderCell>
+					</AKTableHeader>
+					{connections?.map((connection) => (
+						<AKTableRow key={connection.connectionId}>
+							<AKTableCell>{connection.name}</AKTableCell>
+							<AKTableCell>{connection.connectionId}</AKTableCell>
+						</AKTableRow>
+					))}
+				</AKTable>
+			) : (
+				<AKTableMessage>{translate().t("reactApp.connections.noConnectionsFound")}</AKTableMessage>
+			)}
+			{delayedLoading && <AKTableMessage>{translate().t("reactApp.general.loading")}</AKTableMessage>}
 		</AKModal>
 	);
 };
