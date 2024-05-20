d974d3f412972d89fe65366a9b60a976020660f3
Ronen Mars <ronen@autokitteh.com>
2024-05-20 13:11:57 +0300

    feat: reconnection - refactor: react component overlay

--- vscode-react/src/components/atoms/overlay.component.tsx
+++ vscode-react/src/components/atoms/overlay.component.tsx
@@ -3,7 +3,10 @@ type OverlayProps = {
 	isVisibile: boolean;
 	className?: string;
 };
-export const Overlay = ({ onOverlayClick = () => {}, isVisibile, className }: OverlayProps) => {
-	const overlayClass = `absolute h-screen w-screen top-0 left-0 bg-black opacity-50 z-50 ${className}`;
-	return isVisibile && <div className={overlayClass} onClick={() => onOverlayClick()}></div>;
-};
+export const Overlay = ({ onOverlayClick = () => {}, isVisibile, className }: OverlayProps) =>
+	isVisibile && (
+		<div
+			className={`absolute h-screen w-screen top-0 left-0 bg-black opacity-50 z-50 ${className}`}
+			onClick={() => onOverlayClick()}
+		></div>
+	);
