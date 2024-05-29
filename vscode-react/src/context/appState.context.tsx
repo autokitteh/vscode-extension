6a64ba0de82105cdfba6b54db7b143b61e0a49f7
Ronen Mars <ronen@autokitteh.com>
2024-05-29 16:10:16 +0300

    fix: increase loader delay and remove loading state from the sessions view

--- vscode-react/src/context/appState.context.tsx
+++ vscode-react/src/context/appState.context.tsx
@@ -37,7 +37,7 @@ export const AppStateProvider = ({ children }: { children: ReactNode }) => {
 	useEffect(() => {
 		let timeoutId: NodeJS.Timeout | undefined;
 		if (state.loading) {
-			timeoutId = setTimeout(() => setDelayedLoading(true), 1000);
+			timeoutId = setTimeout(() => setDelayedLoading(true), 1500);
 		} else {
 			clearTimeout(timeoutId);
 			setDelayedLoading(false);
