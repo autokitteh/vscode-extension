16dc2e440babb6b84fe4574e154dc89856a959a5
Ronen Mars <ronen@autokitteh.com>
2024-06-19 15:12:48 +0300

    ci: adjust deploy workflow to bump a version

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	branches: ["main", { name: "rc", prerelease: "true" }],
+	branches: ["main", { name: "rc", prerelease: true }],
 	plugins: [
 		[
 			"@semantic-release/commit-analyzer",
