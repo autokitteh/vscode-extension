20f28ad2c06cf8e5cfd6aaf52ce9c117b36eb811
Ronen Mars <ronen@autokitteh.com>
2024-06-19 17:37:35 +0300

    ci: fix publish workflow (#333)

--- release.config.js
+++ release.config.js
@@ -5,6 +5,7 @@ module.exports = {
 			"@semantic-release/commit-analyzer",
 			{
 				preset: "conventionalcommits",
+				releaseRules: [{ message: "*", release: "patch" }],
 			},
 		],
 		"@semantic-release/release-notes-generator",
