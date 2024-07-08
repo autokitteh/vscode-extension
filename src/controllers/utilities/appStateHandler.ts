082e44e25181165cf2d294f99ecc88eb9a7caba9
Ronen Mars <ronen@autokitteh.com>
2024-07-08 20:57:18 +0300

    refactor: work with workspace settings

--- src/controllers/utilities/appStateHandler.ts
+++ src/controllers/utilities/appStateHandler.ts
@@ -1,18 +1,16 @@
-import { ConfigurationTarget, workspace } from "vscode";
+import { commands } from "vscode";
 
-import { namespaces } from "@constants";
+import { namespaces, vsCommands } from "@constants";
 import { LoggerService } from "@services";
 
 export class AppStateHandler {
 	static async set(isEnabled: boolean): Promise<void> {
-		await workspace.getConfiguration().update("autokitteh.serviceEnabled", isEnabled, ConfigurationTarget.Global);
+		await commands.executeCommand(vsCommands.setContext, "serviceEnabled", isEnabled);
 		LoggerService.info(namespaces.appStateHandler, `App state is ${isEnabled ? "enabled" : "disabled"}`);
 	}
 
 	static async get(): Promise<boolean> {
-		const appState = (await workspace
-			.getConfiguration()
-			.get("autokitteh.serviceEnabled", ConfigurationTarget.Global)) as unknown as boolean;
+		const appState = (await commands.executeCommand(vsCommands.getContext, "serviceEnabled")) as unknown as boolean;
 
 		return appState;
 	}
--- src/vscommands/setToken.vscommands.ts
+++ src/vscommands/setToken.vscommands.ts
@@ -1,4 +1,4 @@
-import { commands, ConfigurationTarget, window, workspace } from "vscode";
+import { commands, window } from "vscode";
 
 import { vsCommands } from "@constants";
 import { translate } from "@i18n";
@@ -7,9 +7,7 @@ export const setToken = async () => {
 	const inputToken = await window.showInputBox({ prompt: translate().t("token.enterToken") });
 
 	if (inputToken) {
-		const config = workspace.getConfiguration();
-		await config.update("autokitteh.authToken", inputToken, ConfigurationTarget.Workspace);
-
+		await commands.executeCommand(vsCommands.setContext, "authToken", inputToken);
 		commands.executeCommand(vsCommands.showInfoMessage, translate().t("token.saved"));
 	} else {
 		commands.executeCommand(vsCommands.showErrorMessage, translate().t("token.emtpy"));
