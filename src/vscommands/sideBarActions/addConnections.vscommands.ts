476a68f52a58baa274bfbaf562de4547919b5170
Ronen Mars <ronen@autokitteh.com>
2024-07-15 15:20:45 +0300

    refactor: remove open connections page button from the sidebar

--- src/vscommands/sideBarActions/addConnections.vscommands.ts
+++
@@ -1,8 +0,0 @@
-import * as vscode from "vscode";
-
-import { AK_FRONTEND_URL } from "@constants";
-
-export const openAddConnectionsPage = () => {
-	const integrationsURL = `${AK_FRONTEND_URL}/i/`;
-	vscode.env.openExternal(vscode.Uri.parse(integrationsURL));
-};
