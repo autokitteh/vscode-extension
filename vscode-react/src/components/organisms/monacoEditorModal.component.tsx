a82145adcd743857857b7fb4a68bf644c8f7fbf4
Ronen Mars <ronen@autokitteh.com>
2024-07-15 15:52:02 +0300

    refactor: re-design close button in modals

--- vscode-react/src/components/organisms/monacoEditorModal.component.tsx
+++ vscode-react/src/components/organisms/monacoEditorModal.component.tsx
@@ -7,7 +7,7 @@ import { Modal } from "@react-components/molecules";
 export const MonacoEditorModal = ({ onClose, content }: { onClose: () => void; content?: string }) => (
 	<Modal wrapperClasses={["!bg-transparent z-50"]} classes={["bg-black-semi-transparent", "rounded-none"]}>
 		<div className="flex justify-end text-white font-extrabold pt-8 text-xl">
-			<div className="flex justify-end pt-4 mr-6">
+			<div className="flex justify-end pt-4">
 				<CloseIcon fill="white" onClick={() => onClose()} className="w-4 p-0 cursor-pointer" />
 			</div>
 		</div>
