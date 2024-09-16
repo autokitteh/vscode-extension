734cd9509f21a6108b12d26fbf88c6cfe44be6f5
Vitaliy Stukalo <workinvs@gmail.com>
2024-09-16 20:05:01 +0300

    fix: refactor processRuntime function and fix singleshot dropdown crash (#374)
    Co-authored-by: Ronen Mars <ronen@autokitteh.com>

--- src/controllers/utilities/convertBuildRuntimesToViewTriggers.utils.ts
+++ src/controllers/utilities/convertBuildRuntimesToViewTriggers.utils.ts
@@ -1,20 +1,28 @@
 import { namespaces } from "@constants";
 import { LoggerService } from "@services";
-import { BuildInfoRuntimes, EntrypointTrigger, SessionEntrypoint } from "@type/models";
+import { BuildInfoRuntimes, SessionEntrypoint } from "@type/models";
 
 const processRuntime = (runtime: BuildInfoRuntimes): Record<string, SessionEntrypoint[]> => {
 	const result: Record<string, SessionEntrypoint[]> = {};
-	const filesNames = Object.keys(runtime.artifact.compiled_data);
+	const fileNames = Object.keys(runtime.artifact.compiled_data).filter((fileName) => fileName !== "archive");
 
-	filesNames.forEach((fileName) => {
-		result[fileName] = (result[fileName] || []).concat(
-			runtime.artifact.exports
-				.filter((entrypoint: EntrypointTrigger) => entrypoint.location.path === fileName)
-				.map((entrypoint: EntrypointTrigger) => ({
-					...entrypoint.location,
-					name: entrypoint.symbol,
-				}))
-		);
+	fileNames.forEach((fileName) => {
+		const seen = new Set();
+
+		result[fileName] = runtime.artifact.exports
+			.filter(({ location: { path }, symbol: name }) => path === fileName && !name.startsWith("_"))
+			.map(({ location, symbol: name }) => ({
+				...location,
+				name,
+			}))
+			.filter(({ path, row, col, name }) => {
+				const key = `${path}:${row}:${col}:${name}`;
+				if (seen.has(key)) {
+					return false;
+				}
+				seen.add(key);
+				return true;
+			});
 	});
 
 	return result;
@@ -24,17 +32,16 @@ export const convertBuildRuntimesToViewTriggers = (
 	runtimes: BuildInfoRuntimes[]
 ): Record<string, SessionEntrypoint[]> => {
 	try {
-		const pythonRuntime = runtimes.find((runtime) => runtime.info.name === "python");
-		if (pythonRuntime) {
-			return processRuntime(pythonRuntime);
-		}
+		const supportedRuntimes = ["python", "starlark"];
 
-		const starlarkRuntime = runtimes.find((runtime) => runtime.info.name === "starlark");
-		if (starlarkRuntime) {
-			return processRuntime(starlarkRuntime);
+		const runtime = runtimes.find((runtime) => supportedRuntimes.includes(runtime.info.name));
+
+		if (runtime) {
+			return processRuntime(runtime);
 		}
 	} catch (error) {
 		LoggerService.error(namespaces.buildRuntimeEntrypoints, (error as Error).message);
+
 		return {};
 	}
 
