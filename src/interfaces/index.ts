7d069d65ab3b9ed2f8153cc9bffeb19e5fe4136b
Ronen Mars <ronen@autokitteh.com>
2024-10-06 16:11:30 +0300

    chore: remove unused axios library (#383)

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,3 +1,2 @@
-export type { IApiClient } from "@interfaces/apiClient.interface";
 export type { IProjectView, ProjectViewDelegate, ConnectionsViewDelegate } from "@interfaces/projectView.interface";
 export type { ISidebarView } from "@interfaces/sidebar.interface";
