81304a0dbf80dd9b00114d84a4c1b024f45ef8a9
Ronen Mars <ronen@autokitteh.com>
2024-10-20 07:21:03 +0300

    refactor: refactor interface with optimized code

--- vscode-react/src/interfaces/incomingMessagesHandler.interface.ts
+++ vscode-react/src/interfaces/incomingMessagesHandler.interface.ts
@@ -3,16 +3,16 @@ import { DeploymentSectionViewModel, SessionSectionViewModel } from "@models";
 import { Connection } from "@type/models";
 
 export interface IIncomingMessagesHandler {
-	setTheme?(theme: Theme | undefined): void;
-	setProjectName?(projectName: string | undefined): void;
+	setTheme?(theme?: Theme): void;
+	setProjectName?(projectName?: string): void;
 	setResourcesDir?(projectFolder: string): void;
-	setEntrypoints?: (value: string[] | undefined) => void;
+	setEntrypoints?: (value?: string[]) => void;
 	startLoader?: () => void;
 	stopLoader?: () => void;
-	setDeploymentsSection?: (value: DeploymentSectionViewModel | undefined) => void;
-	setSelectedDeploymentId?: (selectDeploymentId: string | undefined) => void;
-	setSelectedSession?: (sessionId: string | undefined) => void;
-	setSessionsSection?: (sessions: SessionSectionViewModel | undefined) => void;
+	setDeploymentsSection?: (value?: DeploymentSectionViewModel) => void;
+	setSelectedDeploymentId?: (selectDeploymentId?: string) => void;
+	setSelectedSession?: (sessionId?: string) => void;
+	setSessionsSection?: (sessions?: SessionSectionViewModel) => void;
 	setPathResponse?: (pathResponse: boolean) => void;
 	setRetryCountdown?: (countdown: string) => void;
 	setConnections?: (connections: Connection[]) => void;
