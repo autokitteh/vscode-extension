33921144f8eea5f75e81a24f9e4ac98866801aab
Ronen Mars <ronen@autokitteh.com>
2024-10-20 17:17:19 +0300

    feat: add connections web-ui url getter

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -4,4 +4,28 @@ export const baseURLFromVSCode: string = WorkspaceConfig.getFromWorkspace<string
 export const webUIURLFromVSCode: string = WorkspaceConfig.getFromWorkspace<string>("webInterfaceURL", "");
 
 export const BASE_URL = ValidateURL(baseURLFromVSCode) ? baseURLFromVSCode : "";
-export const WEB_UI_URL = ValidateURL(webUIURLFromVSCode) ? webUIURLFromVSCode : "";
+
+function determineWebUIURL(baseURL: string, configuredWebUIURL: string): string {
+	if (!ValidateURL(baseURL)) {
+		return "http://localhost:9982";
+	}
+
+	const url = new URL(baseURL);
+
+	if (url.hostname === "api.autokitteh.cloud") {
+		return "https://app.autokitteh.cloud";
+	}
+
+	if (url.hostname.endsWith("-api.autokitteh.cloud")) {
+		const subdomain = url.hostname.split("-api")[0];
+		return `https://${subdomain}.autokitteh.cloud`;
+	}
+
+	if (ValidateURL(configuredWebUIURL)) {
+		return configuredWebUIURL;
+	}
+
+	return "http://localhost:9982";
+}
+
+export const WEB_UI_URL = determineWebUIURL(BASE_URL, webUIURLFromVSCode);
